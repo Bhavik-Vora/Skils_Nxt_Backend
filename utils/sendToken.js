c006290538c4bead801497849a46ca91802f8f5f
Bhavik Vora <bhavikbvorak56@gmail.com>
2024-09-07 15:27:31 +0530

    Updating routes for security

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,15 +1,15 @@
 import jwt from "jsonwebtoken";
 
-export const sendToken = (res, user,message,statusCode) => {
+export const sendToken = (res, user,message,statusCode=200) => {
   const token = jwt.sign({_id:user._id},process.env.JWT_SECRET,{
     expiresIn:'7d',
   });
 
   const options = {
-    maxAge:  15 * 24 * 60 * 60 * 1000,
+    maxAge:  7 * 24 * 60 * 60 * 1000,
     httpOnly:true,
     secure:true,
-    sameSite:"none",
+    sameSite:"None",
   };
   res.status(statusCode).cookie("cookietoken", token, options).json({
     success: true,
