a4b2916856421a750aa6d06bac1d00baf324e4a2
Bhavik Vora <bhavikbvorak56@gmail.com>
2024-09-08 10:08:07 +0530

    Updating routes for security

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,16 +1,20 @@
 import jwt from "jsonwebtoken";
 
-export const sendToken = (res, user,message,statusCode=200) => {
-  const token = jwt.sign({_id:user._id},process.env.JWT_SECRET,{
-    expiresIn:'7d',
+// Utility function to determine if the environment is production
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const sendToken = (res, user, message, statusCode = 200) => {
+  const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {
+    expiresIn: '7d',
   });
 
   const options = {
-    maxAge:  7 * 24 * 60 * 60 * 1000,
-    httpOnly:true,
-    secure:true,
-    sameSite:"None",
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: isProduction, // Set secure to true only in production
+    sameSite: isProduction ? 'None' : 'Lax', // Set SameSite attribute based on environment
   };
+
   res.status(statusCode).cookie("cookietoken", token, options).json({
     success: true,
     message,
