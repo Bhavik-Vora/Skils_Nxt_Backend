9d421971eddfdda23c3d2a2f810b1196b09f0f3e
Bhavik Vora <bhavikbvorak56@gmail.com>
2024-09-08 15:55:33 +0530

    Updating SENDTOKEN.js_

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,19 +1,15 @@
-import jwt from "jsonwebtoken";
+export const cookieOptions = {
+  maxAge: 15 * 24 * 60 * 60 * 1000,
+  sameSite: "none", // Adjust according to your requirements
+  httpOnly: true,
+  secure: true,
+};
 
-export const sendToken = (res, user,message,statusCode=200) => {
-  const token = jwt.sign({_id:user._id},process.env.JWT_SECRET,{
-    expiresIn:'7d',
-  });
-
-  const options = {
-    maxAge:  7 * 24 * 60 * 60 * 1000,
-    httpOnly:true,
-    secure:true,
-    sameSite:"None",
-  };
-  res.status(statusCode).cookie("cookietoken", token, options).json({
+export const sendToken = (res, user, code, message) => {
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+  return res.status(code).cookie("cookietoken", token, cookieOptions).json({
     success: true,
-    message,
     user,
+    message,
   });
 };
