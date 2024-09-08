e1d3c90ec4d2e21b5a5b2df81a05779d05e514e1
Bhavik Vora <bhavikbvorak56@gmail.com>
2024-09-08 15:15:55 +0530

    Deleting secureurl and secure from sendtoken_2

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,20 +1,16 @@
 import jwt from "jsonwebtoken";
 
-// Utility function to determine if the environment is production
-const isProduction = process.env.NODE_ENV === 'production';
-
-export const sendToken = (res, user, message, statusCode = 200) => {
-  const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {
-    expiresIn: '7d',
+export const sendToken = (res, user,message,statusCode=200) => {
+  const token = jwt.sign({_id:user._id},process.env.JWT_SECRET,{
+    expiresIn:'7d',
   });
 
   const options = {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    // httpOnly: true,
-    // secure: isProduction, // Set secure to true only in production
-    // sameSite: isProduction ? 'None' : 'Lax', // Set SameSite attribute based on environment
+    maxAge:  7 * 24 * 60 * 60 * 1000,
+    httpOnly:true,
+    secure:true,
+    sameSite:"None",
   };
-
   res.status(statusCode).cookie("cookietoken", token, options).json({
     success: true,
     message,
